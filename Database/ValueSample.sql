f5b2f7c6659f28dddded03434d2a65a871ab1094
MinhTranTuan12 <minhttse172842@fpt.edu.vn>
2023-09-24 09:25:12 +0700

    Update data for Have table

--- Database/ValueSample.sql
+++ Database/ValueSample.sql
@@ -952,7 +952,34 @@ insert into Answer(QuestionID, isCorrect, AnswerImage, AnswerText) values
 ('200' , 1 , 'none' , N'Giảm tốc độ chờ xe container rẽ xong rồi tiếp tục đi.'),
 ('200' , 0 , 'none' , N'Vượt về phía bên trái để đi tiếp.');
 go
-
+insert into
+  dbo.Have (QuizID, QuestionID)
+values
+  (1, 1),
+  (1, 2),
+  (1, 3),
+  (1, 4),
+  (1, 5),
+  (1, 6),
+  (1, 7),
+  (1, 8),
+  (1, 9),
+  (1, 10),
+  (1, 11),
+  (1, 12),
+  (1, 13),
+  (1, 14),
+  (1, 15),
+  (1, 16),
+  (1, 17),
+  (1, 18),
+  (1, 19),
+  (1, 20),
+  (1, 21),
+  (1, 22),
+  (1, 23),
+  (1, 24),
+  (1, 25)
 -----------------------------------------[ QUERY TEST ]-----------------------------------------------------
 /*
 use DrivingLicense;
