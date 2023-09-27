89293e375079f572b89d251dc7a62465be7c8e95
MinhTranTuan12 <minhttse172842@fpt.edu.vn>
2023-09-27 11:53:41 +0700

    Update style for admin page

--- Demo2/JS/testing.js
+++ Demo2/JS/testing.js
@@ -5,11 +5,11 @@ const themeToggler = document.querySelector(".theme-toggler");
 
 menuBTN.addEventListener("click", () => {
     sideMenu.style.display = "block";
-})
+});
 
 closeBTN.addEventListener("click", () => {
     sideMenu.style.display = "none";
-})
+});
 
 
 //Change theme
@@ -18,5 +18,17 @@ themeToggler.addEventListener("click", () => {
 
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-})
+    changeBSTheme();
+});
 
+//Change bootstrap theme
+
+function changeBSTheme() {
+    var htmlTag = document.getElementsByTagName('html')[0];
+    var theme = htmlTag.getAttribute('data-bs-theme');
+    if (theme === 'light') {
+        htmlTag.setAttribute('data-bs-theme', 'dark');
+    } else {
+        htmlTag.setAttribute('data-bs-theme', 'light');
+    }
+}
