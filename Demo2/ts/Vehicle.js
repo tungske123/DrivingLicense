7cd66abd664cae5635e75bdbda7ff93be75a0a8d
MinhTranTuan12 <minhttse172842@fpt.edu.vn>
2023-10-22 12:04:14 +0700

    Update Vehicle CRUD Filter features

--- Demo2/ts/Vehicle.js
+++ Demo2/ts/Vehicle.js
@@ -10,11 +10,19 @@ var VehicleRequestData = /** @class */ (function () {
         this.startPrice = -1;
         this.endPrice = -1;
     }
+    VehicleRequestData.prototype.reset = function () {
+        this.keyword = '';
+        this.types = [];
+        this.brands = [];
+        this.startPrice = -1;
+        this.endPrice = -1;
+    };
     return VehicleRequestData;
 }());
 var vehicleTableBody = document.getElementById('vehicleTableBody');
 var vehicleSearch = document.getElementById('vehicleSearch');
 var tableLoader = document.getElementById('tableLoader');
+var createModal = document.getElementById('createProductModal');
 var page = 1;
 var sendData = new VehicleRequestData();
 function fetchVehiclesData() {
@@ -43,29 +51,171 @@ function getFormattedPrice(price) {
 function renderVehicleTable(vehicleList) {
     var template = document.getElementById('vehicle-row-template');
     vehicleTableBody.innerHTML = "";
-    vehicleList.forEach(function (vehicle) {
-        var clone = document.importNode(template.content, true);
-        var cells = clone.querySelectorAll('td');
-        cells[0].textContent = vehicle.name;
-        cells[1].textContent = vehicle.brand;
-        cells[2].textContent = vehicle.years.toString();
-        cells[3].textContent = vehicle.type;
-        cells[4].textContent = '...';
-        cells[5].textContent = getFormattedPrice(vehicle.rentPrice);
-        vehicleTableBody.appendChild(clone);
-    });
-    // tableLoader.style.display = 'none';
-    // tableLoader.style.display = 'block';
-    // setTimeout(() => {
-    // }, 1000);
+    tableLoader.style.display = 'block';
+    setTimeout(function () {
+        vehicleList.forEach(function (vehicle) {
+            var clone = document.importNode(template.content, true);
+            var cells = clone.querySelectorAll('td');
+            cells[0].textContent = vehicle.name;
+            cells[1].textContent = vehicle.brand;
+            cells[2].textContent = vehicle.years.toString();
+            cells[3].textContent = vehicle.type;
+            cells[4].textContent = '...';
+            cells[5].textContent = getFormattedPrice(vehicle.rentPrice);
+            var dropdownButton = cells[6].querySelector('.dropdown_button');
+            var dropDownContent = cells[6].querySelector('.dropdown_content');
+            dropdownButton.addEventListener('click', function () {
+                var hasDropdown = (!dropDownContent.classList.contains('hidden') && dropDownContent.classList.contains('block'));
+                if (!hasDropdown) {
+                    dropDownContent.classList.remove('hidden');
+                    dropDownContent.classList.add('block');
+                }
+                else {
+                    dropDownContent.classList.add('hidden');
+                    dropDownContent.classList.remove('block');
+                }
+            });
+            var editButton = dropDownContent.querySelector('.edit_btn');
+            var detailsButton = dropDownContent.querySelector('.details_btn');
+            var deleteButton = dropDownContent.querySelector('.cancel_btn');
+            editButton.addEventListener('click', function () {
+                toggleUpdateModal();
+            });
+            detailsButton.addEventListener('click', function () {
+                toggleDetailsModal();
+            });
+            deleteButton.addEventListener('click', function () {
+                toggleDeleteModal();
+            });
+            vehicleTableBody.appendChild(clone);
+        });
+        tableLoader.style.display = 'none';
+    }, 1000);
 }
-function renderVehicleTableRow() {
+function toggleUpdateModal() {
+    var updateModal = document.getElementById('updateProductModal');
+    var openedUpdateModal = (!updateModal.classList.contains('hidden') && updateModal.classList.contains('flex') && updateModal.classList.contains('blur-background'));
+    if (!openedUpdateModal) {
+        updateModal.classList.remove('hidden');
+        updateModal.classList.add('flex');
+        updateModal.classList.add('blur-background');
+    }
+    else {
+        updateModal.classList.add('hidden');
+        updateModal.classList.remove('flex');
+        updateModal.classList.remove('blur-background');
+    }
+}
+function toggleDetailsModal() {
+    var detailsModal = document.getElementById('readProductModal');
+    var openedDetaisModal = (!detailsModal.classList.contains('hidden') && detailsModal.classList.contains('flex') && detailsModal.classList.contains('blur-background'));
+    if (!openedDetaisModal) {
+        detailsModal.classList.remove('hidden');
+        detailsModal.classList.add('flex');
+        detailsModal.classList.add('blur-background');
+    }
+    else {
+        detailsModal.classList.add('hidden');
+        detailsModal.classList.remove('flex');
+        detailsModal.classList.remove('blur-background');
+    }
+}
+function toggleDeleteModal() {
+    var deleteModal = document.getElementById('deleteModal');
+    var openedDeleteModal = (!deleteModal.classList.contains('hidden') && deleteModal.classList.contains('flex') && deleteModal.classList.contains('blur-background'));
+    if (!openedDeleteModal) {
+        deleteModal.classList.remove('hidden');
+        deleteModal.classList.add('flex');
+        deleteModal.classList.add('blur-background');
+    }
+    else {
+        deleteModal.classList.add('hidden');
+        deleteModal.classList.remove('flex');
+        deleteModal.classList.remove('blur-background');
+    }
+}
+function formatPrice(price) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+function reverseFormatPrice(price) {
+    // Remove all dots from the string and convert it to a number
+    return parseInt(price.replace(/\./g, ""));
 }
 window.addEventListener('DOMContentLoaded', function () {
     fetchVehiclesData();
+});
+var priceInputs = document.querySelectorAll('.price_input');
+var startPriceInput = document.getElementById('start_price');
+var endPriceInput = document.getElementById('end_price');
+var typeCheckList = document.querySelectorAll('.type_check');
+var brandCheckList = document.querySelectorAll('.brand_check');
+var resetButton = document.getElementById('resetButton');
+function resetFilter() {
+    startPriceInput.value = '0';
+    endPriceInput.value = '0';
+    typeCheckList.forEach(function (typeCheck) {
+        typeCheck.checked = false;
+    });
+    brandCheckList.forEach(function (brandCheck) {
+        brandCheck.checked = false;
+    });
+    sendData.reset();
+    fetchVehiclesData();
+}
+priceInputs.forEach(function (priceInput) {
+    priceInput.addEventListener('input', function () {
+        var priceValue = Number(priceInput.value.replace(/\D/g, ''));
+        priceInput.value = priceValue.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        var startPrice = reverseFormatPrice(startPriceInput.value);
+        var endPrice = reverseFormatPrice(endPriceInput.value);
+        if (startPrice >= endPrice) {
+            alert('Vui lòng nhập giá phù hợp');
+            startPriceInput.value = '0';
+            endPriceInput.value = '0';
+        }
+    });
+});
+startPriceInput.addEventListener('input', function () {
+    var startPrice = reverseFormatPrice(startPriceInput.value);
+    sendData.startPrice = startPrice;
+    fetchVehiclesData();
+});
+endPriceInput.addEventListener('input', function () {
+    var endPrice = reverseFormatPrice(endPriceInput.value);
+    sendData.endPrice = endPrice;
+    fetchVehiclesData();
+});
+typeCheckList.forEach(function (typeCheck) {
+    typeCheck.addEventListener('input', function () {
+        if (typeCheck.checked) {
+            sendData.types.push(String(typeCheck.value));
+        }
+        else {
+            var index = sendData.types.indexOf(String(typeCheck.value));
+            if (index !== -1) {
+                sendData.types.splice(index, 1);
+            }
+        }
+        fetchVehiclesData();
+    });
+});
+brandCheckList.forEach(function (brandCheck) {
+    brandCheck.addEventListener('input', function () {
+        if (brandCheck.checked) {
+            sendData.brands.push(String(brandCheck.value));
+        }
+        else {
+            var index = sendData.brands.indexOf(String(brandCheck.value));
+            if (index !== -1) {
+                sendData.brands.splice(index, 1);
+            }
+        }
+        fetchVehiclesData();
+    });
 });
 vehicleSearch.addEventListener('input', function () {
     var newKeyword = vehicleSearch.value;
     sendData.keyword = newKeyword;
     fetchVehiclesData();
 });
+resetButton.addEventListener('click', resetFilter);
--- Demo2/ts/Vehicle.ts
+++ Demo2/ts/Vehicle.ts
@@ -16,10 +16,19 @@ class VehicleRequestData {
     brands: string[] = [];
     startPrice: number = -1;
     endPrice: number = -1;
+    
+    reset() {
+        this.keyword = '';
+        this.types = [];
+        this.brands = [];
+        this.startPrice = -1;
+        this.endPrice = -1;
+    }
 }
 let vehicleTableBody = document.getElementById('vehicleTableBody') as HTMLTableSectionElement;
 const vehicleSearch = document.getElementById('vehicleSearch') as HTMLInputElement;
 const tableLoader = document.getElementById('tableLoader') as HTMLDivElement;
+const createModal = document.getElementById('createProductModal') as HTMLDivElement;
 var page = 1;
 var sendData: VehicleRequestData = new VehicleRequestData();
 function fetchVehiclesData() {
@@ -50,26 +59,181 @@ function getFormattedPrice(price: number) {
 function renderVehicleTable(vehicleList: Vehicle[]) {
     let template = document.getElementById('vehicle-row-template') as HTMLTemplateElement;
     vehicleTableBody.innerHTML = ``;
-    vehicleList.forEach(vehicle => {
-        let clone = document.importNode(template.content, true);
+    tableLoader.style.display = 'block';
+    setTimeout(() => {
+        vehicleList.forEach(vehicle => {
+            let clone = document.importNode(template.content, true);
+    
+            let cells = clone.querySelectorAll('td') as NodeListOf<HTMLTableCellElement>;
+            cells[0].textContent = vehicle.name;
+            cells[1].textContent = vehicle.brand;
+            cells[2].textContent = vehicle.years.toString();
+            cells[3].textContent = vehicle.type;
+            cells[4].textContent = '...';
+            cells[5].textContent = getFormattedPrice(vehicle.rentPrice);
+            const dropdownButton = cells[6].querySelector('.dropdown_button') as HTMLButtonElement;
+            var dropDownContent = cells[6].querySelector('.dropdown_content') as HTMLDivElement;
+            dropdownButton.addEventListener('click', () => {
+                const hasDropdown: boolean = (!dropDownContent.classList.contains('hidden') && dropDownContent.classList.contains('block'));
+                if (!hasDropdown) {
+                    dropDownContent.classList.remove('hidden');
+                    dropDownContent.classList.add('block');
+                } else {
+                    dropDownContent.classList.add('hidden');
+                    dropDownContent.classList.remove('block');
+                }
+            });
+            const editButton = dropDownContent.querySelector('.edit_btn') as HTMLButtonElement;
+            const detailsButton = dropDownContent.querySelector('.details_btn') as HTMLButtonElement;
+            const deleteButton = dropDownContent.querySelector('.cancel_btn') as HTMLButtonElement;
+            editButton.addEventListener('click', () => {
+                toggleUpdateModal();
+            });
+            detailsButton.addEventListener('click', () => {
+                toggleDetailsModal();
+            });
+            deleteButton.addEventListener('click', () => {
+                toggleDeleteModal();
+            });
+            vehicleTableBody.appendChild(clone);
+        });
+        tableLoader.style.display = 'none';
+    }, 1000);
+}
 
-        let cells = clone.querySelectorAll('td') as NodeListOf<HTMLTableCellElement>;
-        cells[0].textContent = vehicle.name;
-        cells[1].textContent = vehicle.brand;
-        cells[2].textContent = vehicle.years.toString();
-        cells[3].textContent = vehicle.type;
-        cells[4].textContent = '...';
-        cells[5].textContent = getFormattedPrice(vehicle.rentPrice);
+function toggleUpdateModal() {
+    const updateModal = document.getElementById('updateProductModal') as HTMLDivElement;
+    const openedUpdateModal = (!updateModal.classList.contains('hidden') && updateModal.classList.contains('flex') && updateModal.classList.contains('blur-background'));
+    if (!openedUpdateModal) {
+        updateModal.classList.remove('hidden');
+        updateModal.classList.add('flex');
+        updateModal.classList.add('blur-background');
+    } else {
+        updateModal.classList.add('hidden');
+        updateModal.classList.remove('flex');
+        updateModal.classList.remove('blur-background');
+    }
+}
 
-        vehicleTableBody.appendChild(clone);
-    });
+function toggleDetailsModal() {
+    const detailsModal = document.getElementById('readProductModal') as HTMLDivElement;
+    const openedDetaisModal = (!detailsModal.classList.contains('hidden') && detailsModal.classList.contains('flex') && detailsModal.classList.contains('blur-background'));
+    if (!openedDetaisModal) {
+        detailsModal.classList.remove('hidden');
+        detailsModal.classList.add('flex');
+        detailsModal.classList.add('blur-background');
+    } else {
+        detailsModal.classList.add('hidden');
+        detailsModal.classList.remove('flex');
+        detailsModal.classList.remove('blur-background');
+    }
+}
+
+function toggleDeleteModal() {
+    const deleteModal = document.getElementById('deleteModal') as HTMLDivElement;
+    const openedDeleteModal = (!deleteModal.classList.contains('hidden') && deleteModal.classList.contains('flex') && deleteModal.classList.contains('blur-background'));
+    if (!openedDeleteModal) {
+        deleteModal.classList.remove('hidden');
+        deleteModal.classList.add('flex');
+        deleteModal.classList.add('blur-background');
+    } else {
+        deleteModal.classList.add('hidden');
+        deleteModal.classList.remove('flex');
+        deleteModal.classList.remove('blur-background');
+    }
+}
+function formatPrice(price: number) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+function reverseFormatPrice(price: string): number {
+    // Remove all dots from the string and convert it to a number
+    return parseInt(price.replace(/\./g, ""));
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     fetchVehiclesData();
 });
+
+const priceInputs = document.querySelectorAll('.price_input') as NodeListOf<HTMLInputElement>;
+const startPriceInput = document.getElementById('start_price') as HTMLInputElement;
+const endPriceInput = document.getElementById('end_price') as HTMLInputElement;
+const typeCheckList = document.querySelectorAll('.type_check') as NodeListOf<HTMLInputElement>;
+const brandCheckList = document.querySelectorAll('.brand_check') as NodeListOf<HTMLInputElement>;
+const resetButton = document.getElementById('resetButton') as HTMLButtonElement;
+
+function resetFilter() {
+    startPriceInput.value = '0';
+    endPriceInput.value = '0';
+    typeCheckList.forEach(typeCheck => {
+        typeCheck.checked = false;
+    });
+    brandCheckList.forEach(brandCheck => {
+        brandCheck.checked = false;
+    });
+    sendData.reset();
+    fetchVehiclesData();
+}
+
+
+priceInputs.forEach(priceInput => {
+    priceInput.addEventListener('input', () => {
+        let priceValue: Number = Number(priceInput.value.replace(/\D/g, ''));
+        priceInput.value = priceValue.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        const startPrice = reverseFormatPrice(startPriceInput.value);
+        const endPrice = reverseFormatPrice(endPriceInput.value);
+        if (startPrice >= endPrice) {
+            alert('Vui lòng nhập giá phù hợp');
+            startPriceInput.value = '0';
+            endPriceInput.value = '0';
+        }
+    });
+});
+
+startPriceInput.addEventListener('input', () => {
+    const startPrice = reverseFormatPrice(startPriceInput.value);
+    sendData.startPrice = startPrice;
+    fetchVehiclesData();
+});
+
+endPriceInput.addEventListener('input',() => {
+    const endPrice = reverseFormatPrice(endPriceInput.value);
+    sendData.endPrice = endPrice;
+    fetchVehiclesData();
+});
+
+typeCheckList.forEach(typeCheck => {
+    typeCheck.addEventListener('input', () => {
+        if (typeCheck.checked) {
+            sendData.types.push(String(typeCheck.value));
+        } else {
+            const index = sendData.types.indexOf(String(typeCheck.value));
+            if (index !== -1) {
+                sendData.types.splice(index, 1);
+            }  
+        }
+        fetchVehiclesData();
+    });
+});
+
+brandCheckList.forEach(brandCheck => {
+    brandCheck.addEventListener('input', () => {
+        if (brandCheck.checked) {
+            sendData.brands.push(String(brandCheck.value));
+        } else {
+            const index = sendData.brands.indexOf(String(brandCheck.value));
+            if (index !== -1) {
+                sendData.brands.splice(index, 1);
+            }
+        }
+        fetchVehiclesData();
+    });
+});
+
 vehicleSearch.addEventListener('input', () => {
     const newKeyword = vehicleSearch.value;
     sendData.keyword = newKeyword;
     fetchVehiclesData();
 });
+
+resetButton.addEventListener('click', resetFilter);
